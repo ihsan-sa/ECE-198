800a247e1994466dbfa868560c247bb9dfd5b06f
Dhyey Bhatt <dhyey.bhatt@gmail.com>
2024-11-26 01:00:14 -0500

    changed to 8 bits transmitted and read at a time

--- UART_Communication/Core/Src/main.c
+++ UART_Communication/Core/Src/main.c
@@ -63,25 +63,33 @@ int __io_putchar(int ch)
  return ch;
 }
 
-void sendMsg(GPIO_TypeDef* gpioTypeTransmit, int transmitPin, int16_t message, int baudDelay, int bufferDelay){
+void sendMsg(GPIO_TypeDef* gpioTypeTransmit, int transmitPin, int8_t message, int baudDelay, int bufferDelay){
 	// start bit
 	HAL_GPIO_WritePin(gpioTypeTransmit, transmitPin, 0);
 	HAL_Delay(baudDelay);
 
 	// send message
-	for (int i=0; i<16; i++){
-		HAL_GPIO_WritePin(gpioTypeTransmit, transmitPin, !!(message & (1 << 15-i)) );
+	for (int i=0; i<8; i++){
+		HAL_GPIO_WritePin(gpioTypeTransmit, transmitPin, !!(message & (1 << 7-i)) );
+//
+//		if ( !!(message & (1 << 15-i)) ){
+//			printf("1\r\n");
+//		}
+//		else{
+//			printf("0\r\n");
+//		}
+
 		HAL_Delay(baudDelay);
 	}
-
+//	printf("\n");
 	// go back to default high
 	HAL_GPIO_WritePin(gpioTypeTransmit, transmitPin, 1);
 	HAL_Delay(bufferDelay);
 }
 
 
-int16_t readMsg(GPIO_TypeDef* gpioTypeRead, int readPin, int baudDelay){
-	int16_t got_msg = 0;
+int8_t readMsg(GPIO_TypeDef* gpioTypeRead, int readPin, int baudDelay){
+	int8_t got_msg = 0;
 	printf("waiting for start bit\r\n");
 	while(HAL_GPIO_ReadPin(gpioTypeRead, readPin)){
 		//wait for start bit
@@ -89,8 +97,8 @@ int16_t readMsg(GPIO_TypeDef* gpioTypeRead, int readPin, int baudDelay){
 
 	printf("received start bit\r\n");
 	HAL_Delay(baudDelay+10); // let start bit end, delay a bit
-	for (int i=0; i<16; i++){
-		got_msg |= HAL_GPIO_ReadPin(gpioTypeRead, readPin) << 15-i;
+	for (int i=0; i<8; i++){
+		got_msg |= HAL_GPIO_ReadPin(gpioTypeRead, readPin) << 7-i;
 //		if (HAL_GPIO_ReadPin(gpioTypeRead, readPin)){
 //			printf("1");
 //		}
@@ -137,7 +145,7 @@ int main(void)
   MX_GPIO_Init();
   MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
-
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, 1);
 
   /* USER CODE END 2 */
 
@@ -146,7 +154,8 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-	  printf("%u\r\n", readMsg(GPIOA, GPIO_PIN_6, 100) );
+	  sendMsg(GPIOA, GPIO_PIN_7, 0b10100010, 100, 500);
+	  //printf("%u\r\n", readMsg(GPIOA, GPIO_PIN_6, 100) );
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
