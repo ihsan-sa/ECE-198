19010392f1d67b014974a3d4b10e35fdded5a206
Ihsan S. <ihsan.salari@gmail.com>
2024-11-26 19:20:20 -0500

    Created library file for the computation of position from a.

---
+++ Receiver_Unit_Code_ECE198/Core/Inc/compute_pos.h
@@ -0,0 +1,17 @@
+/*
+ * compute_pos.h
+ *
+ *  Created on: Nov 26, 2024
+ *      Author: ihsan
+ *
+ *   The purpose of this file is to compute the position of the system from the acceleration.
+ */
+
+#ifndef INC_COMPUTE_POS_H_
+#define INC_COMPUTE_POS_H_
+
+
+
+
+
+#endif /* INC_COMPUTE_POS_H_ */
