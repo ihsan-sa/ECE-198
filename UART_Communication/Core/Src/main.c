014c98ec215b41b18ab2662611399b8b949c0b0b
Ihsan S. <ihsan.salari@gmail.com>
2024-11-26 19:15:25 -0500

    Moved all UART communications into a seperate header file.

--- UART_Communication/Core/Src/main.c
+++ UART_Communication/Core/Src/main.c
@@ -21,7 +21,7 @@
 #include <stdio.h>
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "uart_d.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -56,65 +56,13 @@ static void MX_USART2_UART_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+
 int __io_putchar(int ch)
 {
 /* Support printf over UART */
 (void) HAL_UART_Transmit(&huart2, (uint8_t *) &ch, 1, 0xFFFFU);
  return ch;
 }
-
-// rn i have GPIO_PIN_7 to be send value
-// rn i have GPIO_PIN_6 to be read value
-
-void sendMsg(GPIO_TypeDef* gpioTypeTransmit, int transmitPin, int8_t message, int baudDelay, int bufferDelay){
-	// start bit
-	HAL_GPIO_WritePin(gpioTypeTransmit, transmitPin, 0);
-	HAL_Delay(baudDelay);
-
-	// send message
-	for (int i=0; i<8; i++){
-		HAL_GPIO_WritePin(gpioTypeTransmit, transmitPin, !!(message & (1 << 7-i)) );
-//
-//		if ( !!(message & (1 << 15-i)) ){
-//			printf("1\r\n");
-//		}
-//		else{
-//			printf("0\r\n");
-//		}
-
-		HAL_Delay(baudDelay);
-	}
-//	printf("\n");
-	// go back to default high
-	HAL_GPIO_WritePin(gpioTypeTransmit, transmitPin, 1);
-	HAL_Delay(bufferDelay);
-}
-
-
-uint8_t readMsg(GPIO_TypeDef* gpioTypeRead, int readPin, int baudDelay){
-	uint8_t got_msg = 0;
-	printf("waiting for start bit\r\n");
-	while(HAL_GPIO_ReadPin(gpioTypeRead, readPin)){
-		//wait for start bit
-	}
-
-	printf("received start bit\r\n");
-	HAL_Delay(baudDelay+10); // let start bit end, delay a bit
-	for (int i=0; i<8; i++){
-		got_msg |= HAL_GPIO_ReadPin(gpioTypeRead, readPin) << 7-i;
-//		if (HAL_GPIO_ReadPin(gpioTypeRead, readPin)){
-//			printf("1");
-//		}
-//		else{
-//			printf("0");
-//		}
-//		printf("\r\n");
-	    HAL_Delay(baudDelay);
-	}
-
-	return got_msg;
-}
-
 /* USER CODE END 0 */
 
 /**
